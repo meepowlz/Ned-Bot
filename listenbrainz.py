621e7ed551734b94d99f859b259deea9e320fc92
meepowlz <meepowlz@gmail.com>
2022-02-11 14:38:30 -0500

    Initial commit

---
+++ listenbrainz.py
@@ -0,0 +1,11 @@
+"""
+API URL: https://api.listenbrainz.org
+DOCS: https://listenbrainz.readthedocs.io/en/latest/users/api/#reference
+
+GOAL: Display Gabi's top recordings
+username: twitch0001
+/1/user/username_here/listen-count
+/1/user/username_here/recordings
+"""
+
+
