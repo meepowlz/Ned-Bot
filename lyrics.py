c8c48cdb6f4803ae6d6f6a328b3c332ccee860c7
meepowlz <meepowlz@gmail.com>
2022-02-14 10:09:38 -0500

    a

--- lyrics.py
+++ lyrics.py
@@ -1,1 +1,13 @@
-#
+# Adds lyrics to a json file
+# Randomly selects a lyric to display
+
+import json
+
+
+lyrics = []
+
+lyrics.append({})
+
+# Open json file
+with open("lyrics.json", "w") as file:
+    json.dumps(lyrics)
