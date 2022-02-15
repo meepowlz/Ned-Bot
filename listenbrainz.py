e954e9e231bcbfbcb9d3abd136c0cb4705f61356
meepowlz <meepowlz@gmail.com>
2022-02-15 14:08:23 -0500

    Update listenbrainz stuff

--- listenbrainz.py
+++ listenbrainz.py
@@ -10,9 +10,18 @@ username: twitch0001
 
 import requests
 
+
+# Gets the response objects..?
 response_listen_count = requests.get("https://api.listenbrainz.org/1/user/twitch0001/listen-count")
 response_recordings = requests.get("https://api.listenbrainz.org/1/stats/user/twitch0001/recordings")
 
-listencount = response_listen_count.json()
+# Converts to json format
+listen_count = response_listen_count.json()["payload"]["count"]
+recordings = response_recordings.json()["payload"]["recordings"]
 
-print(listencount["payload"]["count"])
+
+# Prints total listens and top songs
+print(f"Total listens: {listen_count}")
+for i, recording in enumerate(recordings):
+    print(f"{i}. \"{recording['track_name']}\" by {recording['artist_name']} on \"{recording['release_name']}\"")
+    print(f"Listen count: {recording['listen_count']}")
