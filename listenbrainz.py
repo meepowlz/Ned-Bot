264d0f7525d7f9bd15e4439bfa0639f05d87a080
meepowlz <meepowlz@gmail.com>
2022-02-15 14:32:57 -0500

    Update listenbrainz stuff

--- listenbrainz.py
+++ listenbrainz.py
@@ -14,14 +14,25 @@ import requests
 # Gets the response objects..?
 response_listen_count = requests.get("https://api.listenbrainz.org/1/user/twitch0001/listen-count")
 response_recordings = requests.get("https://api.listenbrainz.org/1/stats/user/twitch0001/recordings")
+response_np = requests.get("https://api.listenbrainz.org/1/user/twitch0001/playing-now")
+response_recent = requests.get("https://api.listenbrainz.org/1/users/twitch0001/recent-listens")
 
 # Converts to json format
 listen_count = response_listen_count.json()["payload"]["count"]
 recordings = response_recordings.json()["payload"]["recordings"]
+now_playing = response_np.json()
+recent_listens = response_recent.json()
+
+print(now_playing)
+print(recent_listens)
 
 
 # Prints total listens and top songs
-print(f"Total listens: {listen_count}")
-for i, recording in enumerate(recordings):
-    print(f"{i}. \"{recording['track_name']}\" by {recording['artist_name']} on \"{recording['release_name']}\"")
-    print(f"Listen count: {recording['listen_count']}")
+def user_stats():
+    print(f"Total listens: {listen_count}")
+    for i, recording in enumerate(recordings):
+        print(f"{i}. \"{recording['track_name']}\" by {recording['artist_name']} on \"{recording['release_name']}\"")
+        print(f"Listen count: {recording['listen_count']}")
+
+
+user_stats()
