61a77523caef513c111537fd968c47832bd6032c
meepowlz <meepowlz@gmail.com>
2022-02-14 19:23:36 -0500

    Add random lyrics generator

--- lyrics.py
+++ lyrics.py
@@ -2,12 +2,19 @@
 # Randomly selects a lyric to display
 
 import json
+import random
+from collections import deque
+
+
+# Create deque for storing recent lyrics
+lyrics_id_deque = deque([], 10)
 
 # Opens json file to read
 with open("lyrics.json", "r") as file:
     lyrics = json.load(file)
 
 
+# Adds new lyrics to the file
 def add_lyrics():
     while True:
         try:
@@ -18,16 +25,34 @@ def add_lyrics():
             break
     add_ids()
 
+
+# Updates lyric ids
 def add_ids():
     for i, lyric in enumerate(lyrics):
         lyric["id"] = i
     write_changes(lyrics)
 
 
+def check_deque(lyric):
+    if lyric["id"] in lyrics_id_deque:
+        return False
+    else:
+        return True
+
+
+# Generates a random lyric
+def random_lyric(lyrics_list):
+    filtered_lyrics = list(filter(check_deque, lyrics_list))
+    selected_lyric = random.choice(filtered_lyrics)
+    print(selected_lyric["lyric"])
+    lyrics_id_deque.append(selected_lyric["id"])
+    return selected_lyric["lyric"]
+
+
+
+# Writes any changes to the file
 def write_changes(changes):
     # Open json file to write
     with open("lyrics.json", "w") as file:
         json.dump(changes, file, indent=4)
 
-
-add_lyrics()
