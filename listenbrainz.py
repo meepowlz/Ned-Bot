fd907cbb7ac41bfcf944c4075acc090ff47ccc8c
meepowlz <meepowlz@gmail.com>
2022-02-15 10:25:25 -0500

    things

--- listenbrainz.py
+++ listenbrainz.py
@@ -5,7 +5,14 @@ DOCS: https://listenbrainz.readthedocs.io/en/latest/users/api/#reference
 GOAL: Display Gabi's top recordings
 username: twitch0001
 /1/user/username_here/listen-count
-/1/user/username_here/recordings
+/1/stats/user/(user_name)/recordings
 """
 
+import requests
 
+response_listen_count = requests.get("https://api.listenbrainz.org/1/user/twitch0001/listen-count")
+response_recordings = requests.get("https://api.listenbrainz.org/1/stats/user/twitch0001/recordings")
+
+listencount = response_listen_count.json()
+
+print(listencount["payload"]["count"])
