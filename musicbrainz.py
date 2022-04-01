c057ca94c25f9c3b6720e159ae6cc1f1bd31e67b
meepowlz <meepowlz@gmail.com>
2022-04-01 10:22:53 -0400

    things

--- musicbrainz.py
+++ musicbrainz.py
@@ -11,7 +11,7 @@ Query parameter "?fmt=json"
 HONNE_MBID = "58b599cd-b1de-45e3-863a-3b54194a0f20"
 NSWY_MBID = "Warm On A Cold Night"
 
-artist_request = requests.get(f"https://musicbrainz.org/ws/2/artist/{HONNE_MBID}?fmt=json")
+artist_request = requests.get(f"https://musicbrainz.org/ws/2/artist/{HONNE_MBID}?inc=releases&fmt=json")
 print(artist_request)
 
 json_return = artist_request.json()
