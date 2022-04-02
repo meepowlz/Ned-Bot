bec6f7fec782c0e8d6799508bb12cf54c74b2136
meepowlz <meepowlz@gmail.com>
2022-04-02 16:34:21 -0400

    cheemse

--- musicbrainz.py
+++ musicbrainz.py
@@ -2,12 +2,15 @@ import requests
 import json
 import time
 
+
 """
 MusicBrainz API root url
 https://musicbrainz.org/ws/2/
 
 Query parameter "?fmt=json"
 """
+
+selected = []
 
 # Get artist data w/ releases
 HONNE_MBID = "58b599cd-b1de-45e3-863a-3b54194a0f20"
@@ -17,55 +20,57 @@ artist_json = artist_request.json()
 #print(artist_json)
 
 
+def convert_length(milliseconds):
+    seconds = int(milliseconds / 1000)
+    minutes, seconds = divmod(seconds, 60)
+    return f"{minutes}:{seconds:02}"
+
+
+def traverse_tracks(tracks):
+    for i, track in enumerate(tracks):
+        # Print the track number, name and length
+        # title, number, length, id, release date
+        title = track['title']
+        number = track['number']
+        length = convert_length(tracks[i]['length'])
+        id = track['recording']['id']
+        release_date = track['recording']['first-release-date']
+        print(f"{number}. {title} - {length}")
+
+
 # Go through each release
-for i, release in enumerate(artist_json["releases"]):
-    # Print release data
-    print(artist_json["releases"][i]["title"])
-    release_mbid = artist_json['releases'][i]['id']
-    print(f"MBID: {release_mbid}")
+def traverse_releases():
+    for i, release in enumerate(artist_json["releases"]):
+        # Print release title and MBID
+        release_title = artist_json['releases'][i]['title']
+        release_mbid = artist_json['releases'][i]['id']
+        print(f"Release: {release_title}")
+        print(f"MBID: {release_mbid}")
 
-    # Pause so MB doesn't get upset
-    time.sleep(1)
+        # Pause so MB doesn't get upset
+        time.sleep(1)
 
-    # Gather tracks from each release
-    release_request = requests.get(f"https://musicbrainz.org/ws/2/release/{release_mbid}?inc=recordings&fmt=json")
-    print(release_request.status_code)
-    release_json = release_request.json()
+        # Gather tracks from each release
+        release_request = requests.get(f"https://musicbrainz.org/ws/2/release/{release_mbid}?inc=recordings&fmt=json")
+        # print(release_request.status_code)
+        release_json = release_request.json()
 
-    #print(release_json)
-    print(f"Packaging: {release_json['packaging']}")
-    print(f"Release Format: {release_json['media'][0]['format']}")
-    cheese = release_json["media"][0]["tracks"]
-    for i, track in enumerate(cheese):
-        print(f"{i}. {cheese[i]['title']}")
-    print("")
+        # Other information about the release
+        print(f"Packaging: {release_json['packaging']}")
+        print(f"Release Format: {release_json['media'][0]['format']}")
+        tracks = release_json["media"][0]["tracks"]
 
-
-
-
+        # Display tracks from release
+        traverse_tracks(tracks)
 
 
+        user_select = input("Select: 1-All tracks 2-Some tracks 3-No tracks")
 
-# artist_request = requests.get(f"https://musicbrainz.org/ws/2/artist/{HONNE_MBID}?inc=releases&fmt=json")
-# release_request = requests.get(f"https://musicbrainz.org/ws/2/release-group/{NSWY_MBID}?inc=releases&fmt=json")
-# print(artist_request)
-# print(release_request)
-#
-# artist_json = artist_request.json()
-# release_json = release_request.json()
-# print(artist_json)
-# print(release_json)
-#
-# print(artist_json["release-groups"][0]["id"])
-#
-# # Retrieve all release groups
-# for i, release in enumerate(artist_json["release-groups"]):
-#     print(artist_json["release-groups"][i]["title"])
-#     print(f"MBID: {artist_json['release-groups'][i]['id']}")
-#     release_group_mbid = artist_json['release-groups'][i]['id']
-#     time.sleep(1)
-#     release_request = requests.get(f"https://musicbrainz.org/ws/2/release-group/{release_group_mbid}?inc=releases&fmt=json")
-#     cheese = release_request.json()
-#     print(cheese)
-#
+        if user_select == "1":
+            selected.append()
+        print("")
+
+
+def write_changes():
+    print("a")
 
