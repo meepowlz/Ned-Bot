4495ed513ae89849cb717f8f6a19d211be6c795f
meepowlz <meepowlz@gmail.com>
2022-04-14 14:38:27 -0400

    cheemse

--- cogs/dictionary.py
+++ cogs/dictionary.py
@@ -18,11 +18,13 @@ def search_word(query=None):
 	search_request = requests.get(f"https://www.dictionaryapi.com/api/v3/references/collegiate/json/{query}?key={os.environ['MW_DICTIONARY']}")
 	if 200 <= search_request.status_code < 300:
 		results = search_request.json()
-		print(search_request.status_code)
-		print(results)
-		format_results(results)
+		# print(search_request.status_code)
+		# print(results)
 		if not results:
 			print(f"No words were found which matched the search query '{query}'")
+		else:
+			words = format_results(results)
+			display_results(words)
 	else:
 		print("Search failed")
 
@@ -30,25 +32,31 @@ def search_word(query=None):
 def format_results(results):
 
 	words = []
-	# for i, word in enumerate(results):
-	# 	terms = word['meta']['stems']
-	# 	hwi = word.get("hwi", {})
-	# 	prs = hwi.get("prs", [False])[0]
-	# 	print(prs)
-	# 	pronunciation = prs.get("mw", )
-	# 	part_of_speech = word.get("fl")
-	# 	definitions = word.get("shortdef")
-	# 	print(definitions)
-	# 	word_data = {
-	# 		'terms': terms,
-	# 		'pronunciation': pronunciation,
-	# 		'part': part_of_speech,
-	# 		'def': definitions
-	# 	}
-	# 	print()
-	# 	print(word_data)
-	# 	words.append(word_data)
+	for i, word in enumerate(results):
+		terms = word['meta']['stems']
+		hwi = word.get("hwi", {})
+		prs = hwi.get("prs", [{}])[0]
+		print(prs)
+		pronunciation = prs.get("mw", )
+		part_of_speech = word.get("fl")
+		definitions = word.get("shortdef")
+		print(definitions)
+		word_data = {
+			'terms': terms,
+			'pronunciation': pronunciation,
+			'part': part_of_speech,
+			'def': definitions
+		}
+		print()
+		#print(word_data)
+		words.append(word_data)
+	print(words)
 	return words
+
+
+def display_results(words):
+	for word in words:
+		print(word)
 
 
 search_word()
