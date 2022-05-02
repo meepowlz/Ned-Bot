c0fc1e63ff6cc4eb49c7f02bac11de5bc6466403
meepowlz <meepowlz@gmail.com>
2022-05-02 11:29:52 -0400

    things

--- secret_ext.py
+++ secret_ext.py
@@ -1,6 +1,11 @@
 import requests
+from bs4 import BeautifulSoup
+
+# https://beautiful-soup-4.readthedocs.io/en/latest/#searching-the-tree
 
 train_headcode = 800008
 
 r = requests.get(f"https://www.realtimetrains.co.uk/search/handler?qsearch={train_headcode}&type=detailed")
-print(r.text)
+r = r.text
+soup = BeautifulSoup(r, 'html.parser')
+print(soup.find_all("li", string="UID" in))
