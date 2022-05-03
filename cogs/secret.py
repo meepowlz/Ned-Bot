fd2da42e352ea97ae6c909294f1dbce365a2e984
meepowlz <meepowlz@gmail.com>
2022-05-03 00:18:12 -0400

    Convert to aiohttp requests

--- cogs/secret.py
+++ cogs/secret.py
@@ -6,31 +6,17 @@ import random
 import aiohttp
 import discord
 import dotenv
-import requests
 from bs4 import BeautifulSoup
 from discord.ext import commands
 
 dotenv.load_dotenv()
 
-"""
-API Information
-train: 800008
-https://www.realtimetrains.co.uk/about/developer/pull/docs/
-api.rtt.io/api/v1/
-"""
-
-# Gay train service uid
-service_uid = "L16286"
-
-
 # https://beautiful-soup-4.readthedocs.io/en/latest/#searching-the-tree
-
-train_headcode = 800320
 
 """
 - Raise error if no uid found?
 Should work consistently; determine if the UID shifts places at any point depending on the responses
-headcodes
+identities
 800008 (gay train)
 390119 (full wrap train)
 800320 (random train for testing)
@@ -41,26 +27,25 @@ class ServiceException(Exception):
 	pass
 
 
-async def get_service_uid(headcode):
+async def get_service(identity):
 	"""
-	Looks up train by headcode
-	Parses the returned html to extract the current service uid
+	Looks up train by identity
+	Parses the returned html to extract the current service UID
 	Once UID is retrieved, service information is requested
-	Otherwise, ServiceException raised
-	:param headcode:
-	:return:
+	If no UID found, ServiceException is raised
+	:param identity: str
+	:return: str, json
 	"""
-	url=f"https://www.realtimetrains.co.uk/search/handler?qsearch={headcode}&type=detailed"
+	url=f"https://www.realtimetrains.co.uk/search/handler?qsearch={identity}&type=detailed"
 	# Get the html of resultant page after search
 	async with aiohttp.ClientSession() as session:
 		async with session.get(url) as response:
 			r = await response.text()
 
-		print(r)
 		# Find service uid
 		soup = BeautifulSoup(r, 'html.parser')
 		li = soup.find_all("li")
-		uid = -1
+		uid = None
 		for item in li:
 			text = str(item)
 			if "UID" in text:
@@ -68,10 +53,16 @@ async def get_service_uid(headcode):
 				uid_index = text.find("UID")
 				uid = text[uid_index + 4:].split(",", 1)[0]
 				print(f"UID: {uid}")
+
 		if uid:
-			return session, uid
+			c_datetime = await get_current_datetime()
+			url = f"https://api.rtt.io/api/v1/json/service/{uid}/{c_datetime['year']}/{c_datetime['month']}/{c_datetime['day']}"
+			# Request service information
+			async with session.get(url, auth=aiohttp.BasicAuth(os.environ['RTT_USER'], os.environ['RTT_PASS'])) as response:
+				return uid, await response.json()
 		else:
-			raise ServiceException(f"No service found associated with headcode {headcode}")
+			print("OBJECTION")
+			raise ServiceException(f"No service found associated with identity {identity}")
 
 
 async def get_current_datetime():
@@ -94,7 +85,7 @@ async def get_current_datetime():
 	return split_datetime
 
 
-async def make_request(session, uid, embed):
+async def build_embed(data, identity, uid, embed):
 	"""
 	Requests service information
 	Displays information in an embed
@@ -102,47 +93,41 @@ async def make_request(session, uid, embed):
 	:param embed: discord.Embed
 	:return: discord.Embed
 	"""
-	# Get datetime for url
-	c_datetime = await get_current_datetime()
-	url = f"https://api.rtt.io/api/v1/json/service/{uid}/{c_datetime['year']}/{c_datetime['month']}/{c_datetime['day']}"
-	# Request service information
-	async with session.get(url, auth=aiohttp.BasicAuth(os.environ['RTT_USER'], os.environ['RTT_PASS'])) as response:
-		data = await response.json()
-
 	# Organize returned data
 	operator = data['atocName']
 	origin = data['origin'][0]
 	destination = data['destination'][0]
 
 	# Format information in an embed
-	embed.title = f"{operator} - {uid}"
+	embed.title = f"{operator} - Service UID {uid}"
 	embed.add_field(name="Departure",
 					value=f"Departing {origin['description']} station\n"
 						  f"Time: {origin['publicTime'][0:2]}:{origin['publicTime'][2:4]}")
 	embed.add_field(name="Arrival",
 					value=f"Arriving at {destination['description']} station\n"
 						  f"Time: {destination['publicTime'][0:2]}:{destination['publicTime'][2:4]}")
-	temp = random.randint(1, 3)  # Randomly select image to display
-	embed.set_image(url=os.environ[f'SECRET_IMG_{temp}'])
+	if identity == "800008":
+		temp = random.randint(1, 3)  # Randomly select image to display
+		embed.set_image(url=os.environ[f'SECRET_IMG_{temp}'])
 	embed.timestamp = datetime.datetime.today()
 
 	return embed
 
-asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
-asyncio.run(get_service_uid(train_headcode))
-
-
 
 @commands.command()
-async def secret(ctx: commands.Context, *, train_headcode: str):
+async def secret(ctx: commands.Context, *, identity: str):
 	# Displays embed with service information
-	session, uid = await get_service_uid(train_headcode)
+	try:
+		uid, data = await get_service(identity)
+	except ServiceException as error:
+		return await ctx.send(str(error))
 	base_embed = discord.Embed(color=ctx.author.color)
 	base_embed.set_author(name=f"{ctx.author.display_name} is gay", icon_url=ctx.author.avatar.url)
-	embed = await make_request(session, uid, base_embed)
+	embed = await build_embed(data, identity, uid, base_embed)
 	await ctx.send(embed=embed)
 
 
 async def setup(bot):
+	asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 	bot.add_command(secret)
 
