f8e8cb99eb49b20aa8dff787ada37ad8f630b2e0
meepowlz <meepowlz@gmail.com>
2022-05-03 00:18:15 -0400

    Convert to aiohttp requests

--- secret_ext.py
+++ secret_ext.py
@@ -1,17 +1,25 @@
+import asyncio
+
+import aiohttp
 import requests
 from bs4 import BeautifulSoup
+import discord
 
 # https://beautiful-soup-4.readthedocs.io/en/latest/#searching-the-tree
 
-train_headcode = 800008
+train_headcode = 800320
 
 """
 - Raise error if no uid found?
 Should work consistently; determine if the UID shifts places at any point depending on the responses
+headcodes
+800008 (gay train)
+390119 (full wrap train)
+800320 (random train for testing)
 """
 
 
-def get_service_uid(train_headcode):
+async def get_service_uid(train_headcode):
     """
     Looks up train by headcode
     Parses the returned html to extract the current service uid
@@ -19,9 +27,12 @@ def get_service_uid(train_headcode):
     :param train_headcode:
     :return:
     """
+    url = f"https://www.realtimetrains.co.uk/search/handler?qsearch={train_headcode}&type=detailed"
     # Get the html of resultant page after search
-    r = requests.get(f"https://www.realtimetrains.co.uk/search/handler?qsearch={train_headcode}&type=detailed")
-    r = r.text
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            r = await response.text()
+    print(r)
     # Find service uid
     soup = BeautifulSoup(r, 'html.parser')
     li = soup.find_all("li")
@@ -29,11 +40,11 @@ def get_service_uid(train_headcode):
     for item in li:
         text = str(item)
         if "UID" in text:
-            print(text)
+            #print(text)
             uid_index = text.find("UID")
             uid = text[uid_index + 4:].split(",", 1)[0]
-            print(uid)
+            print(f"UID: {uid}")
     return uid
 
-
-print(get_service_uid(train_headcode))
+asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+asyncio.run(get_service_uid(train_headcode))
