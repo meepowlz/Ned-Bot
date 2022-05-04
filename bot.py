52ea74f259cc9fae7cd5ad4434d135d4fa951ed7
meepowlz <meepowlz@gmail.com>
2022-05-03 23:56:07 -0400

    Add image to embed

--- bot.py
+++ bot.py
@@ -12,12 +12,13 @@ bot = commands.Bot(command_prefix=os.environ["PREFIX"], intents=intents)
 bot.lyrics_channel_id = int(os.environ["LYRICS_CHANNEL_ID"])
 bot.songvs_channel_id = int(os.environ["SONGVS_CHANNEL_ID"])
 
+
 @bot.event
 async def on_ready():
 	await bot.load_extension("cogs.cheese")
 	await bot.load_extension("cogs.dictionary")
 	await bot.load_extension("cogs.pilots")
-	await bot.load_extension("cogs.secret")
+	await bot.load_extension("cogs.rtt")
 	await bot.load_extension("cogs.songvs")
 	await bot.load_extension("jishaku")
 
